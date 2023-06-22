9ede04335593f720c9b8aba87bfff47cf712ee25
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-06-21 22:04:13 -0300

    Adiciona página show de cada oficina

--- Controllers/OficinaController.cs
+++ Controllers/OficinaController.cs
@@ -1,29 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Revis.Models;
-using Revis.Repositorio;
+using System.Linq;
 
 namespace Revis.Controllers
 {
     public class OficinaController : Controller
     {
-
+        Contexto contexto = new Contexto();
         public IActionResult Index()
         {
-            return View();
+           
+            try
+            {
+                List<OficinaModel> oficinas = (from OficinaModel p in contexto.Oficinas select p).Include(ofc => ofc.mecanicos).ToList<OficinaModel>();
+                /*foreach (OficinaModel item in oficinas)
+                {
+                    Console.WriteLine(item.nome);
+                    foreach (Email itemMecanico in item.mecanicos)
+                    {
+
+                        Console.WriteLine("\t" + itemMecanico.email);
+
+                    }
+                    Console.WriteLine();
+                }*/
+                return View(oficinas);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return View();
+
+            }
         }
 
         public IActionResult Create()
         {
             return View();
         }
-
-        public IActionResult Update()
+        public IActionResult Show(int id)
         {
-            return View();
-        }
+            OficinaModel oficina = contexto.Oficinas.Find(id);
+            if (oficina == null)
+            {
+                return View("Error");
+            }
+            List<MecanicoModel> mecanicos = contexto.Mecanicos.Where(m => m.oficina.id == id).ToList();
 
-        public IActionResult Delete()
-        {
+            ViewBag.Oficina = oficina;
+            ViewBag.Mecanicos = mecanicos;
             return View();
         }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using Revis.Repositorio;
+
 
 namespace Revis
 {
