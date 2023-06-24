513e7c0195d9043996ca6e0452069e8c2abf0120
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2023-06-24 19:02:40 -0300

    Primeiros passos com a busca avançada

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Revis.Models;
+
+namespace Revis.Controllers
+{
+    public class SearchController : Controller
+    {
+        Contexto contexto = new Contexto();
+        public IActionResult AdvancedSearch()
+        {
+            var searchModel = new SearchModel();
+            return View(searchModel);
+        }
+
+        // Action para processar a busca avançada
+        [HttpPost]
+        public IActionResult AdvancedSearch(SearchModel searchModel)
+        {
+            List<OficinaModel> oficinasResult = new List<OficinaModel>();
+            if (!searchModel.OficinaNome.IsNullOrEmpty())
+            {
+                oficinasResult.AddRange(contexto.Oficinas.Where(o => o.nome.Contains(searchModel.OficinaNome)).ToList());
+            }
+            if (!searchModel.OficinaCpnj.IsNullOrEmpty())
+            {
+                oficinasResult.AddRange(contexto.Oficinas.Where(o => o.cpnj.Contains(searchModel.OficinaCpnj)).ToList());
+            }
+            if (!searchModel.OficinaCep.IsNullOrEmpty())
+            {
+                oficinasResult.AddRange(contexto.Oficinas.Where(o => o.cep.Contains(searchModel.OficinaCep)).ToList());
+            }
+            if (!searchModel.OficinaEndereco.IsNullOrEmpty())
+            {
+                oficinasResult.AddRange(contexto.Oficinas.Where(o => o.endereco.Contains(searchModel.OficinaEndereco)).ToList());
+            }
+            if (!searchModel.OficinaCidade.IsNullOrEmpty())
+            {
+                oficinasResult.AddRange(contexto.Oficinas.Where(o => o.cidade.Contains(searchModel.OficinaCidade)).ToList());
+            }
+            if (!searchModel.OficinaEstado.IsNullOrEmpty())
+            {
+                var oficinas = contexto.Oficinas.Where(o => o.estado.Contains(searchModel.OficinaEstado)).ToList();
+                oficinasResult.AddRange(oficinas);
+            }
+
+            List<MecanicoModel> mecanicosResult = new List<MecanicoModel>();
+            if (!searchModel.MecanicoNome.IsNullOrEmpty())
+            {
+                mecanicosResult.AddRange(contexto.Mecanicos.Where(m => m.nome.Contains(searchModel.MecanicoNome)).ToList());
+            }
+            if (!searchModel.MecanicoSexo.IsNullOrEmpty())
+            {
+                mecanicosResult.AddRange(contexto.Mecanicos.Where(m => m.sexo.Contains(searchModel.MecanicoSexo)).ToList());
+            }
+            if (!searchModel.MecanicoCategoriaDeManutencao.IsNullOrEmpty())
+            {
+                mecanicosResult.AddRange(contexto.Mecanicos.Where(m => m.categoriaDeManutencao.Contains(searchModel.MecanicoCategoriaDeManutencao)).ToList());
+            }
+            if (!searchModel.MecanicoResumo.IsNullOrEmpty())
+            {
+                mecanicosResult.AddRange(contexto.Mecanicos.Where(m => m.resumo.Contains(searchModel.MecanicoResumo)).ToList());
+            }
+
+
+            var searchResults = new SearchModel
+            {
+                oficinas = oficinasResult,
+                mecanicos = mecanicosResult
+            };
+
+               return View("AdvancedResults", searchResults);
+        }
+
+
+        public IActionResult AdvancedResults(SearchModel searchModel)
+        {
+            return View(searchModel);
+        }
+    }
+}
---
+++ Models/SearchModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Revis.Models
+{
+    public class SearchModel
+    {
+        public List<OficinaModel> oficinas { get; set; }
+        public List<MecanicoModel> mecanicos { get; set; }
+
+        // Oficinas
+        public string? OficinaNome { get; set; }
+        public string? OficinaCpnj { get; set; }
+        public string? OficinaCep { get; set; }
+        public string? OficinaEndereco { get; set; }
+        public string? OficinaCidade { get; set; }
+        public string? OficinaEstado { get; set; }
+
+        // Mecanicos //
+        public string? MecanicoNome { get; set; }
+        public string? MecanicoCpf { get; set; }
+        public string? MecanicoSexo { get; set; }
+        public int? MecanicoIdade { get; set; }
+        public string? MecanicoCategoriaDeManutencao { get; set; }
+        public string? MecanicoResumo { get; set; }
+    }
+}
